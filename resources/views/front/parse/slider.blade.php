086affc00cb48260d5581951bddb1d37c308a360
Jacek <kontakt@jk-design.pl>
2024-01-29 03:01:25 +0100

    Aktualnosci

--- resources/views/front/parse/slider.blade.php
+++ resources/views/front/parse/slider.blade.php
@@ -4,11 +4,13 @@
         <ul class="list-unstyled mb-0">
     @foreach ($list as $p)
             <li>
-                <picture>
-                    <source type="image/webp" srcset="{{asset('uploads/gallery/images/webp/'.$p->file_webp) }}">
-                    <source type="image/jpeg" srcset="{{asset('uploads/gallery/images/'.$p->file) }}">
-                    <img src="{{asset('uploads/gallery/images/'.$p->file) }}" alt="{{ $p->name }}">
-                </picture>
+                <a href="/uploads/gallery/images/{{$p->file}}" class="swipebox" rel="gallery-{{$p->gallery_id}}" title="">
+                    <picture>
+                        <source type="image/webp" srcset="{{asset('uploads/gallery/images/webp/'.$p->file_webp) }}">
+                        <source type="image/jpeg" srcset="{{asset('uploads/gallery/images/'.$p->file) }}">
+                        <img src="{{asset('uploads/gallery/images/'.$p->file) }}" alt="{{ $p->name }}">
+                    </picture>
+                </a>
             </li>
     @endforeach
         </ul>
