00ba23ad30ee84ebce0cc2b9bcb29c88dd62617a
Jacek <kontakt@jk-design.pl>
2024-01-30 17:19:31 +0100

    Osiedle Slow - Lokalizacja

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -23,7 +23,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/styles.min.css?v=3001') }}" rel="stylesheet">
+    <link href="{{ asset('/css/styles.min.css?v=3001a') }}" rel="stylesheet">
 
     @stack('style')
 
