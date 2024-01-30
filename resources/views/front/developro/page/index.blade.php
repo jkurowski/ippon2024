4e2f416d30fdb7ac9d94e13663453c1935714c74
Jacek <kontakt@jk-design.pl>
2024-01-30 10:00:02 +0100

    Osiedle Slow - Lokalizacja

--- resources/views/front/developro/page/index.blade.php
+++ resources/views/front/developro/page/index.blade.php
@@ -25,7 +25,7 @@
             </div>
         </div>
 
-    @if($investment_page->contact_form)
+        @if($investment_page->contact_form)
             <div class="container mt-5 pt-5">
                 <div class="row">
                     <div class="col-12 text-center">
@@ -33,28 +33,28 @@
                     </div>
                 </div>
             </div>
-        @include('front.contact.form', [ 'page_name' => $investment->name.' - Kontakt'])
-    @endif
+            @include('front.contact.form', [ 'page_name' => $investment->name.' - Kontakt'])
+        @endif
 
-    @if($investment_page->cta_text && $investment_page->cta_button && $investment_page->cta_link)
-    <div id="cta">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h3 class="georgia text-center">{{ $investment_page->cta_text }}</h3>
+        @if($investment_page->cta_text && $investment_page->cta_button && $investment_page->cta_link)
+            <div id="cta">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="georgia text-center">{{ $investment_page->cta_text }}</h3>
+                        </div>
+                    </div>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-6 d-flex justify-content-center align-items-start">
+                            <a href="{{ $investment_page->cta_link }}" class="bttn mb-0 mt-3">{{ $investment_page->cta_button }} <i class="ms-5 las la-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row d-flex justify-content-center">
-                <div class="col-6 d-flex justify-content-center align-items-start">
-                    <a href="{{ $investment_page->cta_link }}" class="bttn mb-0 mt-3">{{ $investment_page->cta_button }} <i class="ms-5 las la-arrow-circle-right"></i></a>
-                </div>
-            </div>
-        </div>
+        @else
+            @if(!$investment_page->contact_form)
+                <div class="no-cta"></div>
+            @endif
+        @endif
     </div>
-    @else
-        @if(!$investment_page->contact_form)
-        <div class="no-cta"></div>
-       @endif
-    @endif
-</div>
 @endsection
--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -23,7 +23,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/styles.min.css?v=2901g') }}" rel="stylesheet">
+    <link href="{{ asset('/css/styles.min.css?v=3001') }}" rel="stylesheet">
 
     @stack('style')
 
