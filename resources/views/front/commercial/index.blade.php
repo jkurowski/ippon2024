a363427b199246ab15b199a359cb963985ce8e13
Jacek <kontakt@jk-design.pl>
2024-02-01 03:42:10 +0100

    Komercyjne - RWD

--- resources/views/front/commercial/index.blade.php
+++ resources/views/front/commercial/index.blade.php
@@ -22,14 +22,14 @@
             <div class="container">
                 @foreach($commercials as $c)
                     <div class="{{ $loop->even ? 'row' : 'row flex-row-reverse' }}">
-                        <div class="col-6">
+                        <div class="col-12 col-xl-6">
                             @if($c->gallery_id)
                                 {!! parse_text('[galeria=commercial]'.$c->gallery_id.'[/galeria]') !!}
                             @else
                                 <div class="empty-ippon"></div>
                             @endif
                         </div>
-                        <div class="col-6 d-flex align-items-center">
+                        <div class="col-12 col-xl-6 d-flex align-items-center mt-4 mt-xl-0">
                             <div class="commercials-text">
                                 <h2>{{ $c->name }}</h2>
                                 {!! $c->text !!}
