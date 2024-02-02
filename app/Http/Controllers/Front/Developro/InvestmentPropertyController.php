4ad27be7d681d8ce4a5cd412d3ce8d06a6f8d05d
Jacek <kontakt@jk-design.pl>
2024-02-02 16:33:41 +0100

    Inestycje - karta mieszkania - podobne mieszkania

--- app/Http/Controllers/Front/Developro/InvestmentPropertyController.php
+++ app/Http/Controllers/Front/Developro/InvestmentPropertyController.php
@@ -28,6 +28,7 @@ class InvestmentPropertyController extends Controller
 
         $similar = Property::select('properties.*', 'floors.number as floor_number')
             ->where('rooms', $property->rooms)
+            ->where('id', '<>', $property->id)
             ->join('floors', 'properties.floor_id', '=', 'floors.id')
             ->inRandomOrder()
             ->limit(3)
