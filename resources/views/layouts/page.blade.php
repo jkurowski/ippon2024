fa29cf5c06a8c2ee533aa2708ebc983aea5d085a
Jacek <kontakt@jk-design.pl>
2024-02-02 16:50:46 +0100

    Inestycje - karta mieszkania - formularz kontaktowy

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -23,7 +23,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/styles.min.css?v=0202a') }}" rel="stylesheet">
+    <link href="{{ asset('/css/styles.min.css?v=0202b') }}" rel="stylesheet">
 
     @stack('style')
 
