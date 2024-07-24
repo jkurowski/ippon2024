80ccac17b7878f86744c79322fda1be13f8cb1e4
Jacek <kontakt@jk-design.pl>
2024-07-24 13:43:29 +0200

    Tlumaczenia cz. 6

--- app/Helpers/RentType.php
+++ app/Helpers/RentType.php
@@ -1,18 +1,29 @@
 <?php
 
 if (! function_exists('rentType')) {
-    function rentType(int $number): string
+    function rentType(int $number)
     {
         $types = [
-            1 => 'Lokale mieszkalne',
-            2 => 'Lokale usługowe',
-            3 => 'Hale usługowe',
-            4 => 'Powierzchnia biurowa',
-            5 => 'Powierzchnia magazynowa',
-            6 => 'Powierzchnia handlowo – magazynowa',
-            7 => 'Powierzchnia parkingowa',
+            1 => 'Residential premises',
+            2 => 'Service premises',
+            3 => 'Service halls',
+            4 => 'Office space',
+            5 => 'Warehouse space',
+            6 => 'Commercial and storage space',
+            7 => 'Parking area',
         ];
 
-        return $types[$number] ?? '';
+        if (app()->getLocale() == 'pl') {
+            $types = [
+                1 => 'Lokale mieszkalne',
+                2 => 'Lokale usługowe',
+                3 => 'Hale usługowe',
+                4 => 'Powierzchnia biurowa',
+                5 => 'Powierzchnia magazynowa',
+                6 => 'Powierzchnia handlowo – magazynowa',
+                7 => 'Powierzchnia parkingowa',
+            ];
+        }
+        return $types[$number] ?? null;
     }
 }
