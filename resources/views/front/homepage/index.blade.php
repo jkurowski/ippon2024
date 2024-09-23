0fb01c03b5274655a9d3b76b7265c61dd0e288e3
Jacek <kontakt@jk-design.pl>
2024-09-23 12:30:26 +0200

    Mieszkania w promocji

--- resources/views/front/homepage/index.blade.php
+++ resources/views/front/homepage/index.blade.php
@@ -175,7 +175,7 @@
             <div data-aos="fade-right" data-aos-offset="400" data-aos-delay="0" class="mb-4 mb-xl-0 order-1 order-xl-2 col-12 col-xl-6">
                 <div class="invest-item-thumb">
                     <span class="img-badge">{{ investmentStatus(1) }}</span>
-                    <a href="/pl/i/osiedle-slow"><img src="{{ asset('/uploads/files/osiedle-slow/ekologiczne-rozwiazania.jpg') }}" alt="" class="golden-border w-100" width="840" height="760"></a>
+                    <a href="/pl/i/osiedle-slow"><img src="{{ asset('/uploads/files/osiedle-slow/osiedle-slow.jpg') }}" alt="" class="golden-border w-100" width="840" height="760"></a>
                 </div>
             </div>
         </div>
