021c6772b65c347b98fd21fff8d38f3ce2a2f7da
Jacek Kurowski <jacek@4dl.pl>
2025-09-03 01:10:56 +0200

    [Jacek][New] Prospekt w budynkach

--- resources/views/front/investment_property/index.blade.php
+++ resources/views/front/investment_property/index.blade.php
@@ -254,11 +254,14 @@
                                 </div>
                             </div>
                         @endif
-                        @if($investment->file_brochure)
-                            <div class="mt-2 text-end prospekt">
-                                <a href="{{ asset('/investment/brochure/'.$investment->file_brochure) }}" target="_blank" class="link-body-primary small">Prospekt informacyjny</a>
-                            </div>
-                        @endif
+
+                        @auth
+                            @if($investment->file_brochure)
+                                <div class="mt-2 text-end prospekt">
+                                    <a href="{{ asset('/investment/brochure/'.$investment->file_brochure) }}" target="_blank" class="link-body-primary small">Prospekt informacyjny</a>
+                                </div>
+                            @endif
+                        @endauth
                     </div>
                     <div class="pe-0 pe-xl-4">
                         <div class="row mt-5">
