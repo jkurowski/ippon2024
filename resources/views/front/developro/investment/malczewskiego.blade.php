ccd870823426b2dff752bb62416413d4b181d5d3
Jacek <kontakt@jk-design.pl>
2026-06-23 10:59:35 +0200

    [Jacek][New] Inwestycja Malczewskiego

--- resources/views/front/developro/investment/malczewskiego.blade.php
+++ resources/views/front/developro/investment/malczewskiego.blade.php
@@ -10,21 +10,36 @@
 
         <div id="hero">
             <div class="hero-apla">
+                @if($current_locale == 'pl')
                 <h1>PRESTIŻ MIERZY SIĘ SPOKOJEM.</h1>
                 <h5>Luksus ciszy, prestiż lokalizacji w kameralnej części Gdańska</h5>
                 <a href="#contact" class="bttn bttn-icon mt-5">FORMULARZ KONTAKTOWY <i class="ms-3 las la-chevron-circle-right"></i></a>
+                @else
+                    <h1>PRESTIGE BEGINS WITH PEACE.</h1>
+                    <h5>The luxury of quiet living in a prestigious and intimate part of Gdańsk</h5>
+                    <a href="#contact" class="bttn bttn-icon mt-5">CONTACT FORM <i class="ms-3 las la-chevron-circle-right"></i></a>
+                @endif
             </div>
             <img src="{{ asset('images/hero-malczewskiego.jpg') }}" alt="" class="w-100">
         </div>
 
         <nav id="invest-nav">
             <ul>
+                @if($current_locale == 'pl')
                 <li><a href="#about">Opis inwestycji</a></li>
                 <li><a href="#premium">Premium</a></li>
                 <li><a href="#location">Lokalizacja</a></li>
                 <li><a href="#standard">Standard</a></li>
                 <li><a href="#nature">Spójnie z naturą</a></li>
                 <li><a href="#contact">Kontakt</a></li>
+                @else
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#premium">Premium</a></li>
+                    <li><a href="#location">Location</a></li>
+                    <li><a href="#standard">Standard</a></li>
+                    <li><a href="#nature">Consistent with nature</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                @endif
             </ul>
         </nav>
 
@@ -37,12 +52,21 @@
                     </div>
                     <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                         <div class="section-text ps-0 ps-lg-5 mt-4 mt-lg-0">
+                            @if($current_locale == 'pl')
                             <h3>O inwestycji</h3>
                             <h2>Nowoczesna <br>interpretacja klasyki</h2>
                             <p>Architektura naszego osiedla w subtelny sposób nawiązuje do historycznej tkanki Gdańska. Wyważone proporcje, przemyślany rytm elewacji oraz starannie dobrane, szlachetne materiały tworzą nowoczesną interpretację tradycyjnych miejskich kamienic.</p>
                             <p>&nbsp;</p>
                             <p>To unikalny projekt, w dzielnicy Siedlce, składający się z zaledwie 3 kameralnych budynków, w których łącznie zaplanowano 118 apartamentów.</p>
                             <a href="#contact" class="bttn bttn-icon mt-4">FORMULARZ KONTAKTOWY <i class="ms-3 las la-chevron-circle-right"></i></a>
+                            @else
+                                <h3>About the Development</h3>
+                                <h2>A Contemporary Interpretation <br>of Timeless Architecture</h2>
+                                <p>The architecture of the development draws subtle inspiration from Gdańsk’s historic urban fabric. Balanced proportions, a carefully composed façade rhythm, and a refined palette of noble materials create a contemporary interpretation of the city’s traditional townhouses.</p>
+                                <p>&nbsp;</p>
+                                <p>Located in the Siedlce district, this distinctive residential development consists of just three intimate buildings, offering a total of 118 apartments. The limited scale of the project ensures a sense of privacy and exclusivity while remaining closely connected to the vibrant character of Gdańsk.</p>
+                                <a href="#contact" class="bttn bttn-icon mt-4">CONTACT FORM <i class="ms-3 las la-chevron-circle-right"></i></a>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -63,8 +87,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 text-center">
+                        @if($current_locale == 'pl')
                         <h2>Udogodnienia Premium</h2>
                         <h3>Twój prywatny azyl. Czas na regenerację.</h3>
+                        @else
+                            <h2>Premium Amenities</h2>
+                            <h3>Your Private Sanctuary. A Space to Recharge.</h3>
+                        @endif
                     </div>
                 </div>
                 <div class="row row-premium">
@@ -75,11 +104,19 @@
                         <div class="ps-0 ps-lg- 5 d-flex justify-content-center gap-5 align-items-center h-100">
                             <div class="premium-icon">
                                 <img src="{{ asset('gdansk/strefa-fitness.png') }}" class="w-100" width="244" height="200" alt="Ikonka strefy fitness">
-                                <p>PRYWATNA<br>STREFA FITNESS</p>
+                                @if($current_locale == 'pl')
+                                    <p>PRYWATNA<br>STREFA FITNESS</p>
+                                @else
+                                    <p>PRYWATNA<br>STREFA FITNESS</p>
+                                @endif
                             </div>
                             <div class="premium-icon">
                                 <img src="{{ asset('gdansk/silownia.png') }}" class="w-100" width="244" height="200" alt="Ikonka strefy fitness">
-                                <p>W PEŁNI WYPOSAŻONA<br>SIŁOWNIA</p>
+                                @if($current_locale == 'pl')
+                                    <p>W PEŁNI WYPOSAŻONA<br>SIŁOWNIA</p>
+                                @else
+                                    <p>W PEŁNI WYPOSAŻONA<br>SIŁOWNIA</p>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -92,11 +129,19 @@
                         <div class="pe-0 pe-lg-5 d-flex justify-content-center gap-5 align-items-center h-100">
                             <div class="premium-icon">
                                 <img src="{{ asset('gdansk/strefa-relaksu.png') }}" class="w-100" width="244" height="200" alt="Ikonka strefy fitness">
-                                <p>ZEWNĘTRZNA<br>STREFA RELAKSU</p>
+                                @if($current_locale == 'pl')
+                                    <p>ZEWNĘTRZNA<br>STREFA RELAKSU</p>
+                                @else
+                                    <p>ZEWNĘTRZNA<br>STREFA RELAKSU</p>
+                                @endif
                             </div>
                             <div class="premium-icon">
                                 <img src="{{ asset('gdansk/ochrona.png') }}" class="w-100" width="244" height="200" alt="Ikonka strefy fitness">
-                                <p>REPREZENTACYJNE<br>LOBBY Z OCHRONĄ</p>
+                                @if($current_locale == 'pl')
+                                    <p>REPREZENTACYJNE<br>LOBBY Z OCHRONĄ</p>
+                                @else
+                                    <p>REPREZENTACYJNE<br>LOBBY Z OCHRONĄ</p>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -112,9 +157,15 @@
                     </div>
                     <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                         <div class="section-text ps-0 ps-lg-5 mt-4 mt-lg-0">
+                            @if($current_locale == 'pl')
                             <h3>Lokalizacja</h3>
                             <h2>Wszystko, czego potrzebujesz. Dokładnie tam, gdzie chcesz.</h2>
                             <p>Lokalizacja przy ul. Malczewskiego to idealny kompromis między dynamicznym rytmem miasta a kojącym spokojem. Gdańskie Siedlce to rejon, który w przeciwieństwie do przesyconego turystami Śródmieścia, zachował znacznie więcej przestrzeni, naturalnej zieleni i kameralnego charakteru.</p>
+                            @else
+                                <h3>Location</h3>
+                                <h2>Everything You Need. Exactly Where You Want It.</h2>
+                                <p>Located on Malczewskiego Street, the development offers the perfect balance between the energy of the city and the comfort of a more peaceful setting. Siedlce is one of Gdańsk’s most desirable residential districts—an area that, unlike the bustling and tourist-filled city centre, has preserved its spacious character, abundant greenery, and sense of privacy.</p>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -139,9 +190,15 @@
                     </div>
                     <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                         <div class="section-text ps-0 ps-lg-5 mt-4 mt-lg-0">
+                            @if($current_locale == 'pl')
                             <h3>Standard</h3>
                             <h2>Zadbaj o zdrowie i relaks bez wychodzenia z domu</h2>
                             <p>Prywatna strefa fitness z saunami pozwala zadbać o zdrowie i regenerację bez wychodzenia z domu, a w pełni wyposażona siłownia spełni oczekiwania nawet najbardziej wymagających.</p>
+                            @else
+                                <h3>Standard</h3>
+                                <h2>Prioritize Wellness and Relaxation Without Leaving Home</h2>
+                                <p>A private fitness and sauna area allows residents to focus on their well-being and recovery without stepping outside, while the fully equipped gym has been designed to meet the expectations of even the most discerning fitness enthusiasts.</p>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -156,10 +213,17 @@
                     </div>
                     <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                         <div class="section-text pe-0 pe-lg-5 mt-4 mt-lg-0">
+                            @if($current_locale == 'pl')
                             <h3>Standard</h3>
                             <h2>Wygoda codziennego życia.</h2>
                             <p>W zaledwie 5 minut spacerem dotrzesz do kluczowych punktów usługowych, sklepów, a także renomowanych szkół i przedszkoli. Mieszkasz w cichej enklawie, zachowując pełną kontrolę nad swoim czasem i logistyką dnia.</p>
                             <a href="#contact" class="bttn bttn-icon mt-4">FORMULARZ KONTAKTOWY <i class="ms-3 las la-chevron-circle-right"></i></a>
+                            @else
+                                <h3>Standard</h3>
+                                <h2>Everyday Convenience, Effortlessly Within Reach</h2>
+                                <p>Within just a five-minute walk, you can access essential services, shops, as well as respected schools and kindergartens. Enjoy the comfort of living in a peaceful enclave while maintaining complete control over your time and the rhythm of your day.</p>
+                                <a href="#contact" class="bttn bttn-icon mt-4">CONTACT FORM <i class="ms-3 las la-chevron-circle-right"></i></a>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -174,10 +238,17 @@
                     </div>
                     <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                         <div class="section-text ps-0 ps-lg-5 mt-4 mt-lg-0">
+                            @if($current_locale == 'pl')
                             <h3>O inwestycji</h3>
                             <h2>Spójnie z naturą.</h2>
                             <p>Kolorystyka osiedla została starannie dobrana, aby harmonijnie wpisywać się w otaczającą naturę. Barwy użyte w materiałach elewacyjnych, detalach architektonicznych i elementach małej architektury nawiązują do tonów ziemi, piasku i zieleni, tworząc spójną i relaksującą wizję całego osiedla.</p>
                             <a href="#contact" class="bttn bttn-icon mt-4">FORMULARZ KONTAKTOWY <i class="ms-3 las la-chevron-circle-right"></i></a>
+                            @else
+                                <h3>About the Development</h3>
+                                <h2>In Harmony with Nature</h2>
+                                <p>The colour palette of the development has been carefully selected to blend seamlessly with its natural surroundings. The tones used in the façade materials, architectural details, and landscape elements draw inspiration from the colours of earth, sand, and greenery, creating a cohesive and calming vision throughout the entire community.</p>
+                                <a href="#contact" class="bttn bttn-icon mt-4">CONTACT FORM <i class="ms-3 las la-chevron-circle-right"></i></a>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -192,11 +263,19 @@
                     </div>
                     <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                         <div class="section-text pe-0 pe-lg-5 mt-4 mt-lg-0">
+                            @if($current_locale == 'pl')
                             <h3>Inwestycja w przygotowaniu</h3>
                             <h2>Tworzymy standard bez kompromisów</h2>
                             <p>Obecnie doopracowujemy detale architektury oraz standard wykończenia, tak aby bezwzględnie sprostać wysokim wymaganiaom przyszłych mieszkańców.</p>
                             <p>&nbsp;</p>
                             <p>Napisz do Nas. Listę apartamentów oraz penthousów udostepnimy w pierwszej kolejności osobom zapisanym na listę oczekujących.</p>
+                            @else
+                                <h3>Development in Preparation</h3>
+                                <h2>Creating a Standard Without Compromise</h2>
+                                <p>We are currently refining every architectural detail and carefully defining the specification of the development to ensure it fully meets the expectations of future residents and delivers an uncompromising living experience.</p>
+                                <p>&nbsp;</p>
+                                <p>Contact us today. The list of available apartments and penthouses will be released first to those who join our priority waiting list.</p>
+                            @endif
                         </div>
                     </div>
                 </div>
